11ddbb3acf470d972a5741658fb0382e40bfe05f
Micah Lindley <hi@micahlindley.com>
2023-08-28 10:34:44 -0500

    Make MyTest more complex

---
+++ other-work/MyTest.java
@@ -0,0 +1,9 @@
+public class MyTest {
+    public static String myName = "Micah";
+    public static void main(String[] args) {
+        sayHello(myName);
+    }
+    public static void sayHello(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
